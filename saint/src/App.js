c014031ff8e4188ae8e45c87b850c4479025719d
ricardobah <rickribeiro99@gmail.com>
2019-01-29 15:50:43 -0300

    configurando server

--- saint/src/App.js
+++ saint/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg'
 import './App.css'
 import {Ola,Flw} from './Ola'
-
+import axios from 'axios'
 class App extends Component {
   constructor(props){
     super(props)
@@ -13,6 +13,7 @@ class App extends Component {
   }  
   componentDidMount(){
     setInterval(()=> this.setState({count: this.state.count+1}),1000)
+    axios.get('http://localhost:3001/genres').then((res)=>console.log(res))
   }
   render() {
     return (
@@ -28,7 +29,7 @@ class App extends Component {
           <div className="collapse navbar-collapse navbar-ex1-collapse">
             <ul className="nav navbar-nav">
               <li>
-                <a href="">Menu item</a>
+                <a href="">Menu item {this.state.count}</a>
               </li>
             </ul>
           </div>
